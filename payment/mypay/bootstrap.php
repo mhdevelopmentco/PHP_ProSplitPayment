0c99d97b0218e1aafc9b162bdf34f3679ac91e1d
mhdevelopmentco <85754399+mhdevelopmentco@users.noreply.github.com>
2021-06-17 06:13:35 +0800

    Update bootstrap.php

--- payment/mypay/bootstrap.php
+++ payment/mypay/bootstrap.php
@@ -29,8 +29,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 // Replace these values by entering your own ClientId and Secret by visiting https://developer.paypal.com/webapps/developer/applications/myapps
-//$clientId = 'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS';
-//$clientSecret = 'EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL';
 
 //TO change: Mendy's ClientId and ClientSecret with Live id and key
 $clientId='';
